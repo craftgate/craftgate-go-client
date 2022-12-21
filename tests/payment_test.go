af1b5b4b6e8339b531c0b12f5b659ee875d96282
Burak Baldırlıoğlu <burak.baldirlioglu@craftbase.io>
2022-12-21 10:52:04 +0300

    revert api credentials to default

--- tests/payment_test.go
+++ tests/payment_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-var paymentClient, _ = craftgate.New("Z65JpMHTT4uZLg9cZFKvaWLZqvWfandE", "HNnUQ3Pc2BGVAR9wlnc2XEbsC1TSKNXZ", "http://localhost:8000")
+var paymentClient, _ = craftgate.New("api-key", "secret-key", "https://sandbox-api.craftgate.io")
 
 func TestPayment_CreatePayment(t *testing.T) {
 	request := adapter.CreatePaymentRequest{
