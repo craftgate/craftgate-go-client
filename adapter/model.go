425f33de8029556526699aa59d10eae396a3ebc0
Emre INAL <9061788+reywyn@users.noreply.github.com>
2025-09-26 15:05:11 +0300

    Prepare for next release v1.0.28 (#155)

--- adapter/model.go
+++ adapter/model.go
@@ -63,7 +63,7 @@ const (
 	ClientVersionHeaderName = "x-client-version"
 	SignatureHeaderName     = "x-signature"
 	AuthVersion             = "1"
-	ClientVersion           = "craftgate-go-client:1.0.27"
+	ClientVersion           = "craftgate-go-client:1.0.28"
 )
 
 // payment type declaration
