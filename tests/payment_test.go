d14e03f79b40af8c4290c1a8cc741a1ed4748842
Mustafa Özcan <wustafa.ozcan@gmail.com>
2025-11-09 21:39:11 +0300

    Update Manuel Refund Service

--- tests/payment_test.go
+++ tests/payment_test.go
@@ -944,7 +944,7 @@ func TestPayment_RefundPaymentTransactionMarkAsRefunded(t *testing.T) {
 	ConversationId := "456d1297-908e-4bd6-a13b-4be31a6e47d5"
 
 	request := adapter.RefundPaymentTransactionMarkAsRefundedRequest{
-		PaymentTransactionId: 1, // change it with a valid paymentId
+		PaymentTransactionId: 1, // change it with a valid PaymentTransactionId
 		ConversationId:       &ConversationId,
 		RefundPrice:          20,
 	}
