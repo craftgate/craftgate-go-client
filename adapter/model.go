a07ddefa999004d5dc81a698dc6bb900baa7c525
furkanozmen <furkan.ozmen@craftgate.io>
2026-02-06 14:13:10 +0300

    Add secure fields token to StoreCardRequest and implement test for storing card

--- adapter/model.go
+++ adapter/model.go
@@ -955,12 +955,12 @@ type RefundWaitingPaymentRequest struct {
 }
 
 type StoreCardRequest struct {
-    CardHolderName string `json:"cardHolderName,omitempty"`
-    CardNumber     string `json:"cardNumber,omitempty"`
-    ExpireYear     string `json:"expireYear,omitempty"`
-    ExpireMonth    string `json:"expireMonth,omitempty"`
-    CardAlias      string `json:"cardAlias,omitempty"`
-    CardUserKey    string `json:"cardUserKey,omitempty"`
+    CardHolderName    string `json:"cardHolderName,omitempty"`
+    CardNumber        string `json:"cardNumber,omitempty"`
+    ExpireYear        string `json:"expireYear,omitempty"`
+    ExpireMonth       string `json:"expireMonth,omitempty"`
+    CardAlias         string `json:"cardAlias,omitempty"`
+    CardUserKey       string `json:"cardUserKey,omitempty"`
     SecureFieldsToken string `json:"secureFieldsToken,omitempty"`
 }
 
